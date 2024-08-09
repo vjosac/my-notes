82cc3dfec6b0a6a28f14277ac51f36afe657c95c
Vjosa Curri <114615502+vjosac@users.noreply.github.com>
2024-08-09 16:03:56 +0200

    remove unused imports

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../firebase_options.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
