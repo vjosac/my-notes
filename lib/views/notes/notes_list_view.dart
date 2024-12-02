b1212ef2f5535afb73c3d33bb59a4a51885af3eb
Vjosa Curri <114615502+vjosac@users.noreply.github.com>
2024-12-02 15:45:13 +0100

    Implement update note functionality and passing arguments on navigation

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -3,14 +3,19 @@ import 'package:mynotes/utilities/dialogs/delete_dialog.dart';
 
 import '../../services/crud/notes_service.dart';
 
-typedef DeleteNoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(DatabaseNote note);
 
 class NotesListView extends StatelessWidget {
   final List<DatabaseNote> notes;
-  final DeleteNoteCallback onDeleteNote;
+  final NoteCallback onDeleteNote;
+  final NoteCallback onTap;
 
-  const NotesListView(
-      {super.key, required this.notes, required this.onDeleteNote});
+  const NotesListView({
+    super.key,
+    required this.notes,
+    required this.onDeleteNote,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +24,9 @@ class NotesListView extends StatelessWidget {
       itemBuilder: (context, index) {
         final note = notes[index];
         return ListTile(
+          onTap: () {
+            onTap(note);
+          },
           title: Text(
             note.text,
             maxLines: 1,
